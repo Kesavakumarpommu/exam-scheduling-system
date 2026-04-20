015ae7f586bb2182d5b06c883c92f7971f8f7240
Kesavakumar pommu <117188076+Kesavakumarpommu@users.noreply.github.com>
2026-04-20 11:55:20 +0530

    Create Result model interface in result.model.ts

---
+++ result.model.ts
@@ -0,0 +1,10 @@
+export interface Result {
+    id: number;
+    studentId: number;
+    examId: number;
+    marks: number;
+    grade: string;
+    gpa: number;
+    status: string;
+    declaredDate: string;
+}
